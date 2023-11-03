f0890fe89f82be5a4691a01d68d9883ba1fdc758
Luis Parrado <luisprmat@gmail.com>
2023-11-03 12:02:29 -0500

    Setup: DB Models and Migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Track;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => 'password',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $tracks = [
+            'Track 1',
+            'Track 2',
+            'Track 3',
+            'Track 4',
+            'Track 5',
+        ];
+        foreach ($tracks as $track) {
+            Track::create(['title' => $track]);
+        }
     }
 }
