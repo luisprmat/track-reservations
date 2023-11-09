698b440f9ba3bacff30ab2a4c26daebc9e9d504c
Luis Parrado <luisprmat@gmail.com>
2023-11-09 12:17:59 -0500

    Filament: Dynamic Reservation Form

---
+++ app/Filament/Resources/ReservationResource.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ReservationResource\Pages;
+use App\Models\Reservation;
+use App\Models\Track;
+use Carbon\CarbonPeriod;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Support\Carbon;
+
+class ReservationResource extends Resource
+{
+    protected static ?string $model = Reservation::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-arrow-down-on-square-stack';
+
+    public static function getModelLabel(): string
+    {
+        return __('reservation');
+    }
+
+    public static function form(Form $form): Form
+    {
+        $dateFormat = 'Y-m-d';
+
+        return $form
+            ->schema([
+                DatePicker::make('date')
+                    ->translateLabel()
+                    ->native(false)
+                    ->minDate(now()->format($dateFormat))
+                    ->maxDate(now()->addWeeks(2)->format($dateFormat))
+                    ->format($dateFormat)
+                    ->required()
+                    ->live(),
+                Radio::make('track')
+                    ->translateLabel()
+                    ->options(fn (Get $get) => self::getAvailableReservations($get))
+                    ->hidden(fn (Get $get) => ! $get('date'))
+                    ->required()
+                    ->columnSpan(2),
+            ]);
+    }
+
+    public static function getAvailableReservations(Get $get): array
+    {
+        $date = Carbon::parse($get('date'));
+        $startPeriod = $date->copy()->hour(14);
+        $endPeriod = $date->copy()->hour(16);
+        $times = CarbonPeriod::create($startPeriod, '1 hour', $endPeriod);
+        $availableReservations = [];
+
+        $tracks = Track::with([
+            'reservations' => function ($q) use ($startPeriod, $endPeriod) {
+                $q->whereBetween('start_time', [$startPeriod, $endPeriod]);
+            },
+        ])
+            ->get();
+
+        foreach ($tracks as $track) {
+            $reservations = $track->reservations->pluck('start_time')->toArray();
+
+            $availableTimes = $times->copy()->filter(function ($time) use ($reservations) {
+                return ! in_array($time, $reservations) && ! $time->isPast();
+            })->toArray();
+
+            foreach ($availableTimes as $time) {
+                $key = $track->id.'-'.$time->format('H');
+                $availableReservations[$key] = $track->title.' '.$time->format('H:i');
+            }
+        }
+
+        return $availableReservations;
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListReservations::route('/'),
+            'create' => Pages\CreateReservation::route('/create'),
+            'edit' => Pages\EditReservation::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/ReservationResource/Pages/CreateReservation.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Resources\ReservationResource\Pages;
+
+use App\Filament\Resources\ReservationResource;
+use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Carbon;
+
+class CreateReservation extends CreateRecord
+{
+    protected static string $resource = ReservationResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $date = Carbon::parse($data['date']);
+        [$trackId, $hour] = explode('-', $data['track']);
+        $startTime = $date->copy()->hour($hour);
+        $endTime = $startTime->copy()->addHour();
+
+        $dateTimeFormat = 'Y-m-d H:i:s';
+
+        return [
+            'user_id' => auth()->id(),
+            'track_id' => $trackId,
+            'start_time' => $startTime->format($dateTimeFormat),
+            'end_time' => $endTime->format($dateTimeFormat),
+        ];
+    }
+}
